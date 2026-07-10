b8c07eabfd87ca056793d206133849b6753b6fb3
dhanraj160307 <rajn81911@gmail.com>
2026-07-10 11:09:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const message = document.getElementById("message");
+
+   
+    if (username === "admin" && password === "12345") {
+        message.style.color = "green";
+        message.textContent = "Login Successful!";
+    } else {
+        message.style.color = "red";
+        message.textContent = "Invalid Username or Password!";
+    }
+});
